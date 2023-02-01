36b42742cb5a2dc6693aa4fb1f1878ac4743e81c
Dana Lobell <danalobell@gmail.com>
2023-02-01 17:34:48 -0500

    Add show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,11 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    rescue ActiveRecord::RecordNotFound
+        render json: "User not found", status: :not_found
+    end
 end
