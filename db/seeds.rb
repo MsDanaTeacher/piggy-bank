fd982d82f70d663ce481e3dd92c32d817a234907
Dana Lobell <danalobell@gmail.com>
2023-02-01 17:07:08 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+User.create!(username: "Dana", password_digest: "abc", avatar: "https://cdn-icons-png.flaticon.com/512/185/185827.png")
+User.create!(username: "Bali", password_digest: "abc", avatar: "https://cdn-icons-png.flaticon.com/128/3093/3093496.png")
